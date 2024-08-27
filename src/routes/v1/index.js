491d9f8835f3c9bcb1fe6ec1fb281b8cea5e3aac
Ayushi Soni <ayushisoni@Ayushis-MacBook-Air.local>
2024-08-27 15:45:59 +0530

    Setup: Added and tested ping API with base initial setup

---
+++ src/routes/v1/index.js
@@ -0,0 +1,8 @@
+const express = require('express');
+const { InfoController } = require('../../controllers');
+
+const v1Router = express.Router();
+
+v1Router.get('/info', InfoController.info);
+
+module.exports = v1Router;
