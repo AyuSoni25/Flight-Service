c2f8e08055a97caeabef00fbea948ed4392d0a3f
Ayushi Soni <ayushisoni@Ayushis-MacBook-Air.local>
2024-08-29 14:14:48 +0530

    Feat: Added complex joins for fetching airport, airplane and city details along with flight details

--- src/repositories/flight.repository.js
+++ src/repositories/flight.repository.js
@@ -1,6 +1,10 @@
 const CrudRepository = require('./crud.repository');
-const { Flight } = require('../models');
+const { Flight, Airplane, Airport, City } = require('../models');
+const { Sequelize } = require('sequelize');
 
+// ORM is definitely good, but we cannot use it for some complex operations
+// and we should always look for the query it is running behind, to check if it the most
+// optimal one, because in some cases the ORM is not optimal and performs slow.
 
 class FlightRepository extends CrudRepository {
     constructor() {
@@ -10,7 +14,38 @@ class FlightRepository extends CrudRepository {
     async getAllFlights(filter, sort) {
         const response = await Flight.findAll({
             where: filter,
-            order: sort
+            order: sort,
+            include: [
+                {
+                    model: Airplane,
+                    required: true,
+                    as: 'airplaneDetail'
+                },
+                {
+                    model: Airport,
+                    required: true,
+                    as: 'departureAirport',
+                    on : {
+                        col1: Sequelize.where(Sequelize.col("Flight.departureAirportId"), "=", Sequelize.col("departureAirport.code"))
+                    },
+                    include: {
+                        model: City,
+                        required: true
+                    }
+                },
+                {
+                    model: Airport,
+                    required: true,
+                    as: 'arrivalAirport',
+                    on : {
+                        col1: Sequelize.where(Sequelize.col("Flight.arrivalAirportId"), "=", Sequelize.col("arrivalAirport.code"))
+                    },
+                    include: {
+                        model: City,
+                        required: true
+                    }
+                }
+            ]
         });
         return response;
     }
